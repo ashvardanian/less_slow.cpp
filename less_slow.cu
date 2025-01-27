986b8bc466307cc98b06d8661f716ac16b0d4e2a
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2025-01-27 13:29:28 +0000

    Fix: Avoid optimizing-out SASS code
    Unless we put an impossible condition with
    a `wmma::store_matrix_sync` the result of
    fragment multiplication is optimized out.

--- less_slow.cu
+++ less_slow.cu
@@ -13,10 +13,13 @@
  *  - What's the Physical Page Caching behavior on GPUs?
  *  - How to schedule advanced computational graphs on GPUs?
  *    A.k.a. CUDA streams vs Graph Node API vs Cooperative Groups?
+ *
+ *  To compile this file, dump the SASS code, and check for Tensor Cores usage:
+ *
+ *      nvcc -arch=sm_70 -Xptxas -v -lineinfo --extra-device-vectorization-info -cubin -o less_slow.cubin less_slow.cu
+ *      cuobjdump -sass less_slow.cubin | grep -i mma
  */
-#pragma once
-#include <cub/cub.cuh>
-#include <cuda_runtime.h>
+#include <cuda_fp16.h> // `half` type
 
 template <typename scalar_type_, std::size_t side_>
 struct small_square_matrix {
@@ -39,8 +42,48 @@ small_square_matrix<scalar_type_, side_> small_matmul_kernel_cuda( //
     return c;
 }
 
-#include <cuda_fp16.h> //`half` type
-#include <mma.h>       // `mma::` intrinsics
+/**
+ *  Starting with Nvidia Volta GPUs, specialized "Tensor Cores" @b (TC) are
+ *  added for faster matrix multiplications. These Tensor Cores are much faster
+ *  than native CUDA implementation of dot-product operations and provide
+ *  special intrinsics for programmers to use.
+ *
+ *  Unlike typical CPU-side intrinsics, in CUDA, C++ templates are used.
+ *  There is not a single Tensor Core generation that natively performs
+ *  @b 16x16x16 FP16 matrix multiplication into FP32 accumulators.
+ *  But we can use @b `wmma` 2D tiles of that size, that will be unpacked
+ *  into the right combination of instructions at compile time.
+ *
+ *  Theoretically, this implies that we could have used 256x256x256 matrices,
+ *  or some other size that optimally fits into the GPU's caches, shared along
+ *  the cores in the same warp, but @b NO! Most sizes won't compile.
+ *
+ *  Moreover, splitting into hardware-specific tile sizes isn't done at the PTX
+ *  level! It's done at the SASS level, so the PTX output for this kernel will
+ *  still contain lines like:
+ *
+ *      wmma.mma.sync.aligned.row.col.m16n16k16.f32.f32 {}, {}, {}, {};
+ *
+ *  That will be lowered to the right SASS instructions by the PTXAS assembler,
+ *  and on Volta SM70 GPUs, will use the only supported size of 8x8x4:
+ *
+ *      HMMA.884.F32.F32.STEP2 R8, R2.reuse.ROW, R2.reuse.COL, R8
+ *
+ *  Unpacking it:
+ *  - HMMA stands for Half-precision Matrix Multiply & Accumulate.
+ *  - 884 stands for the 8x8x4 shape of the matrix multiplication.
+ *  - F32.F32 defines the multiplication and accumulation precision.
+ *  - STEPx denotes the stage of the computation for a specific tile, where
+ *    each HMMA instruction contributes to completing a part of the final
+ *    result. In our case we will get 4 STEPs, repeated 4 times, for a
+ *    total of 16x HMMA instructions per WMMA intrinsic.
+ *
+ *  For optimal usage of Tensor Cores:
+ *  - Ensure your matrix dimensions are multiples of the tile size (8x8x4 on Volta).
+ *  - Use shared memory efficiently to reduce global memory accesses.
+ *  - Properly align input and output matrices in memory (128-byte alignment).
+ */
+#include <mma.h> // `mma::` intrinsics
 
 __global__ void tops_f16_sm70tc_cuda_kernel() {
     using namespace nvcuda;
@@ -48,6 +91,14 @@ __global__ void tops_f16_sm70tc_cuda_kernel() {
     wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::col_major> b_frag;
     wmma::fragment<wmma::accumulator, 16, 16, 16, float> c_frag;
 
-    // To initialize, we can call: `wmma::fill_fragment(c_frag, 0.0f)`
-    wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+    // To initialize, we can call:
+    wmma::fill_fragment(a_frag, __float2half_rn(1.0f));
+    wmma::fill_fragment(b_frag, __float2half_rn(1.0f));
+    wmma::fill_fragment(c_frag, 0.0f);
+
+    // To better saturate the ALU, we could unroll a few iterations:
+    for (int i = 0; i != 128; ++i) wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+
+    // Impossible condition to prevent optimization
+    if (threadIdx.x == -1) wmma::store_matrix_sync(nullptr, c_frag, 16, wmma::mem_row_major);
 }
